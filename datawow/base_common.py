2a8ad3d7c8d73c246bf14e7f07ffac5ce597f76d
NSLog0 <prachaya.su@gmail.com>
2019-03-04 15:20:35 +0700

    change url

--- datawow/base_common.py
+++ datawow/base_common.py
@@ -5,7 +5,7 @@ def base_url(model):
         "videos": "https://kiyo-image.datawow.io/api/v1/",
         "texts":  "https://kiyo-text.datawow.io/api/v1/",
         "ai":     "https://kiyo-image.datawow.io/api/v1/",
-        "docs":   "http://localhost:3001/api/v1/"
+        "docs":   "https://kiyo-image.datawow.io/api/v1/"
     }
 
     return url.get(model, 'Not found')
